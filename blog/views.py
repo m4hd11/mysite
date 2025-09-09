b9f3a234aab13f2c545b630060b7edddb17a2117
mahdi <ip.mahdi99@gmail.com>
2025-09-09 08:44:20 +0330

    template tags
    + use template tags for blog home page and blog single page
    + added counted views features

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,24 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by('-published_date')
     context = {
         'posts': posts
     }
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {
+        'post': post
+    }
+    post.counted_views += 1
+    post.save(update_fields=['counted_views'])
+    return render(request, 'blog/blog-single.html', context)
 
 def test(request, pid):
     # post = Post.objects.get(id=pid)
