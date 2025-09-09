d89048f0d539439940a27e18d164303d10e6c633
mahdi <ip.mahdi99@gmail.com>
2025-09-09 18:18:38 +0330

    make prev post and next post

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 
@@ -10,4 +10,5 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'author')
     # ordering = ['-created_date']
     search_fields = ('title', 'content')
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -12,13 +12,24 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(status=1)
-    post = get_object_or_404(posts, pk=pid)
-    context = {
-        'post': post
-    }
+    posts = list(Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('-published_date'))
+
+    post = get_object_or_404(Post, pk=pid, status=1)
+
     post.counted_views += 1
     post.save(update_fields=['counted_views'])
+
+    current_index = posts.index(post)
+
+    prev_post = posts[current_index - 1] if current_index > 0 else None
+    next_post = posts[current_index + 1] if current_index < len(posts) - 1 else None
+
+    context = {
+        'post': post,
+        'prev_post': prev_post,
+        'next_post': next_post
+    }
+    
     return render(request, 'blog/blog-single.html', context)
 
 def test(request, pid):
