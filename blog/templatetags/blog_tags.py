9d369b72ccab3b5882a9725d59930498a9c20c9d
Mahdi Iranpour <ip.mahdi99@gmail.com>
2025-09-10 19:01:55 +0330

    create new blog tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -12,3 +12,12 @@ def function():
 def function():
     posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg]
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts':posts}  
