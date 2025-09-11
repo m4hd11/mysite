959ade730e54df702f38e1b8922f5e8d6a267ddf
mahdi <ip.mahdi99@gmail.com>
2025-09-11 13:20:34 +0330

    create pagination

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def blog_view(request, **kwargs):
@@ -10,6 +10,14 @@ def blog_view(request, **kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username'):
         posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {
         'posts': posts
     }
