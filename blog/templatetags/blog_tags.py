73dd24d9717164f78dc571a02bd35b5b6be1bc3a
mahdi <ip.mahdi99@gmail.com>
2025-09-12 14:21:33 +0330

    create latest posts template tags
    + create latest posts template tag which show 6 latest posts
    + get categories from posts to show on latest posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -31,3 +31,8 @@ def postcategories():
     for name in categories:
         cat_dict[name] = posts.filter(category=name).count()
     return {'categories': cat_dict}
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(count=3):
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:count]
+    return {'latest_posts': posts}
