46db5a007386e3c5cadbe4dcf44ffe70bf3740b7
mahdi <ip.mahdi99@gmail.com>
2025-09-13 03:35:27 +0330

    create a test contact form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from website.models import Contact
+from website.forms import NameForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -11,3 +13,19 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'website/contact.html')
+
+def test_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(name, subject, email, message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid!')
+        
+    form = NameForm()
+    return render(request, 'test.html', {'form':form})
