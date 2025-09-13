d176aef9b427f80c471b8b610b9bf0d7da21dd06
mahdi <ip.mahdi99@gmail.com>
2025-09-13 17:57:09 +0330

    update contact form
    + save user's name in database as anonymous
    + subject can be empty in contact form
    + create a pop up notification when user submit a ticket

--- website/forms.py
+++ website/forms.py
@@ -2,17 +2,18 @@ from django import forms
 from website.models import Contact, Newsletter
 
 
-class NameForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    subject = forms.CharField(max_length=255)
-    message = forms.CharField(widget=forms.Textarea)
+# class NameForm(forms.Form):
+#     name = forms.CharField(max_length=255)
+#     email = forms.EmailField()
+#     subject = forms.CharField(max_length=255)
+#     message = forms.CharField(widget=forms.Textarea)
 
 
 class ContactForm(forms.ModelForm):
+    subject = forms.CharField(required=False)
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ['name', 'email', 'subject', 'message']
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
