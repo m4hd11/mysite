161070a290278439fe8dd1bfb09fc2b834ca21d3
mahdi <ip.mahdi99@gmail.com>
2025-09-14 13:30:54 +0330

    adding taggit module

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,12 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def blog_view(request, **kwargs):
     posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by('-published_date')
-    if kwargs.get('cat_name'):
+    if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
-    if kwargs.get('author_username'):
+    if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+    if kwargs.get('tag_name') != None:
+        posts = posts.filter(tags__name__in=[kwargs['tag_name']])
     posts = Paginator(posts, 3)
     try:
         page_number = request.GET.get('page')
--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'robots',
     'debug_toolbar',
+    'taggit',
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
 ]
