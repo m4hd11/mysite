bcd5223bd609441844580b2993d1efb11f56fbfc
mahdi <ip.mahdi99@gmail.com>
2025-09-14 13:30:54 +0330

    adding taggit module

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Category(models.Model):
@@ -13,7 +14,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tag
+    tags = TaggableManager()
     category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
@@ -22,7 +23,7 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True) 
 
     class Meta:
-        ordering = ['created_date']
+        ordering = ['-created_date']
         
     def __str__(self):
         return self.title
