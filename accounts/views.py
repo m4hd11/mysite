6c492b40b10d151c795e4a0d7160595940b7aedf
mahdi <ip.mahdi99@gmail.com>
2025-09-15 23:54:30 +0330

    create signin form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def login_View(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
     return render(request, 'accounts/login.html')
 
 # def logout_View(request):
