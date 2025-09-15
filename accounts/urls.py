bb72e78746ef6d19b9a17c1f7b41725ba357af94
mahdi <ip.mahdi99@gmail.com>
2025-09-15 19:18:59 +0330

    create authentication app

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login', views.login_View, name='login'),
+    # path('logout', views.logout_View, name='logout'),
+    path('signup', views.signup_View, name='signup'),
+]
