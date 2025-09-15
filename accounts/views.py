d1d89422ccea196e834f13ef1e366d7568612310
mahdi <ip.mahdi99@gmail.com>
2025-09-16 00:05:01 +0330

    update signin form

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def login_View(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
 
 # def logout_View(request):
 #     return render(request, 'accounts/logout.html')
