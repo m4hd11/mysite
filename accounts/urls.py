355b6012b6c61e7c46a673ef86e3fa6ff01f90c1
mahdi <ip.mahdi99@gmail.com>
2025-09-16 16:03:16 +0330

    create forgot password option
    + adding option which manage users login to account by username or email
    + adding forgot password option and create forgot password templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,45 @@
 from django.urls import path
-from . import views
-
+from .views import login_View, logout_View, signup_View
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login_View, name='login'),
-    path('logout/', views.logout_View, name='logout'),
-    path('signup/', views.signup_View, name='signup'),
+    path('login/', login_View, name='login'),
+    path('logout/', logout_View, name='logout'),
+    path('signup/', signup_View, name='signup'),
+
+    # Forgot Password
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset_form.html',
+            email_template_name='accounts/password_reset_email.html',
+            success_url=reverse_lazy('accounts:password_reset_done')
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
+            success_url=reverse_lazy('accounts:password_reset_complete')
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+
 # Create your views here.
+
+User = get_user_model()
 
 def login_View(request):
     if request.user.is_authenticated:
@@ -13,16 +16,33 @@ def login_View(request):
     next_url = request.POST.get('next') or request.GET.get('next') or '/'
 
     if request.method == "POST":
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect(next_url)
+        username_or_email = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user_obj = User.objects.get(username=username_or_email)
+        except User.DoesNotExist:
+            try:
+                user_obj = User.objects.get(email=username_or_email)
+            except User.DoesNotExist:
+                user_obj = None
+
+        if user_obj:
+            user = authenticate(request, username=user_obj.username, password=password)
+            if user:
+                login(request, user)
+                return redirect(next_url)
+
+        context = {
+            'form': AuthenticationForm(request=request),
+            'error': 'Invalid Username/Email or Password',
+            'next': next_url
+        }
+        return render(request, 'accounts/login.html', context)
+
     else:
         form = AuthenticationForm()
-
-    context = {'form': form, 'next': next_url}
-    return render(request, 'accounts/login.html', context)
+        context = {'form': form, 'next': next_url}
+        return render(request, 'accounts/login.html', context)
     
 @login_required
 def logout_View(request):
