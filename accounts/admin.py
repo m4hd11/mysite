6118f97bad5a9584aa1175f493fd4548bba50418
mahdi <ip.mahdi99@gmail.com>
2025-09-17 17:43:04 +0330

    redisgn
    + adding new posts
    + create profile pivture model for each users
    + redesign logo

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from accounts.models import Profile
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar')
+
+admin.site.register(Profile)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
-from urllib3 import request
 from blog.models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.forms import CommentForm
@@ -13,12 +12,18 @@ from django.utils.http import urlencode
 
 def blog_view(request, **kwargs):
     posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by('-published_date')
+
+
     if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
     if kwargs.get('tag_name') != None:
         posts = posts.filter(tags__name__in=[kwargs['tag_name']])
+
+    last_post = posts.first()
+    last_author = last_post.author if last_post else None
+    
     posts = Paginator(posts, 3)
     try:
         page_number = request.GET.get('page')
@@ -28,7 +33,8 @@ def blog_view(request, **kwargs):
     except EmptyPage:
         posts = posts.get_page(1)
     context = {
-        'posts': posts
+        'posts': posts,
+        'author': last_author,
     }
     return render(request, 'blog/blog-home.html', context)
 
@@ -71,6 +77,7 @@ def blog_single(request, pid):
         'next_post': next_post,
         'comments': comments,
         'form': form,
+        'author': post.author,
     }
     return render(request, 'blog/blog-single.html', context)
 
