0a78569219c7dfbe5648bb0ed1fa3bd3210b9974
mahdi <ip.mahdi99@gmail.com>
2025-09-25 01:10:55 +0330

    comin soon page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 print(">> LOADING mysite/urls.py <<")
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
+from website.views import coming_soon_view
 import debug_toolbar 
 
 sitemaps = {
@@ -29,16 +30,17 @@ sitemaps = {
 }
  
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    path('blog/', include('blog.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, 
-         name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', include('robots.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
-    path('summernote/', include('django_summernote.urls')),
-    path('captcha/', include('captcha.urls')),
+    # path('admin/', admin.site.urls),
+    # path('', include('website.urls')),
+    # path('blog/', include('blog.urls')),
+    # path('accounts/', include('accounts.urls')),
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, 
+    #      name='django.contrib.sitemaps.views.sitemap'),
+    # path('robots.txt', include('robots.urls')),
+    # path('__debug__/', include(debug_toolbar.urls)),
+    # path('summernote/', include('django_summernote.urls')),
+    # path('captcha/', include('captcha.urls')),
+    re_path(r'^.*$', coming_soon_view),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from website.views import *
 app_name= 'website'
 
 urlpatterns = [
-    path('', coming_soon_view, name='index'),
+    path('', index_view, name='index'),
     path('about', about_view, name='about'),
     path('contact', contact_view, name='contact'),
     path('newsletter', newsletter_view, name='newsletter'),
