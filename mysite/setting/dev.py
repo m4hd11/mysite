585c633f2282f61a2f2e71ca902c99e875c2da3d
mahdi <ip.mahdi99@gmail.com>
2025-09-26 23:19:40 +0330

    Update dev.py

--- mysite/setting/dev.py
+++ mysite/setting/dev.py
@@ -3,7 +3,7 @@ from mysite.settings import *
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2l4mi(1f(kv)4(v(7f*-=(v8p_re6ez+vg9j#_d%#zqqn*tqg'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
